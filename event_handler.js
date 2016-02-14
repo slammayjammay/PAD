7855d14cd072bfc983b36c9f569eeba1831afdde
Scott McKenzie <slammayjammay@gmail.com>
2016-02-14 05:17:09 -0800

    Initial commit

---
+++ event_handler.js
@@ -0,0 +1,64 @@
+(function  () {
+  window.Game = function () {
+    this.board = [];
+    for (var i = 0; i < 5; i++) {
+      this.board.push([]);
+    }
+
+    $('#board').on('mousedown', this.mousedown.bind(this));
+    $(window).on('mouseup', this.mouseup.bind(this));
+  };
+
+  Game.prototype.getBoardLocation = function (e) {
+    var top = ~~(e.clientY) - $('#board').offset().top;
+    var left = ~~(e.clientX) - $('#board').offset().left;
+    return [~~(top / 90), ~~(left / 90)];
+  };
+
+  Game.prototype.mousedown = function (e) {
+    e.preventDefault();
+    $(window).on('mousemove', this.mousemove.bind(this));
+
+    this.currentPos = this.getBoardLocation(e);
+    this.showSelectedOrb(e);
+    this.board[this.currentPos[0]][this.currentPos[1]].click();
+
+    $('.orb:not(.clicked, .selected) .swap-region').mouseenter(function (e) {
+      var nextPos = this.getBoardLocation(e);
+      this.swapOrbs(this.currentPos, nextPos);
+      this.currentPos = nextPos;
+    }.bind(this));
+  };
+
+  Game.prototype.mousemove = function (e) {
+    this.$image.css('left', e.clientX - 45 + 'px');
+    this.$image.css('top', e.clientY - 45 + 'px');
+  };
+
+  Game.prototype.mouseup = function () {
+    $(window).off('mousemove');
+    $('.selected').remove();
+    $('.swap-region').unbind('mouseenter');
+    this.board[this.currentPos[0]][this.currentPos[1]].release();
+  };
+
+  Game.prototype.showSelectedOrb = function (e) {
+    var src = Orb.colorUrls[this.board[this.currentPos[0]][this.currentPos[1]].color];
+    this.$image = $('<img>').attr('src', src).addClass('orb selected');
+    this.$image.css('position', 'absolute');
+    this.$image.css('left', e.clientX - 45 + 'px');
+    this.$image.css('top', e.clientY - 45 + 'px');
+    $('body').append(this.$image);
+  };
+
+  Game.prototype.swapOrbs = function (currentPos, nextPos) {
+    var orb1 = this.board[currentPos[0]][currentPos[1]];
+    var orb2 = this.board[nextPos[0]][nextPos[1]];
+
+    this.board[currentPos[0]][currentPos[1]] = orb2;
+    this.board[nextPos[0]][nextPos[1]] = orb1;
+
+    orb1.updatePos(nextPos);
+    orb2.updatePos(currentPos);
+  };
+})();
---
+++ orb.js
@@ -0,0 +1,44 @@
+(function () {
+  window.Orb = function (color, pos) {
+    this.imageWidth = 90;
+    this.color = color;
+    this.pos = pos;
+
+    // create orb html
+    this.$orb = $('<div class="orb">');
+    this.$orb.append($('<div class="swap-region">'));
+    var $image = $('<img>').attr('src', Orb.colorUrls[this.color]);
+    this.$orb.append($image);
+
+    this.addOrbToBoard();
+  }
+
+  Orb.colorUrls = {
+    'r': 'http://pad.dawnglare.com/Red.png',
+    'b': 'http://pad.dawnglare.com/Blue.png',
+    'g': 'http://pad.dawnglare.com/Green.png',
+    'l': 'http://pad.dawnglare.com/Light.png',
+    'd': 'http://pad.dawnglare.com/Dark.png',
+    'h': 'http://pad.dawnglare.com/Heart.png'
+  };
+
+  Orb.prototype.click = function () {
+    this.$orb.css('opacity', 0.3).addClass('clicked');
+  };
+
+  Orb.prototype.addOrbToBoard = function () {
+    this.$orb.css('top', this.imageWidth * this.pos[0] + 'px');
+    this.$orb.css('left', this.imageWidth * this.pos[1] + 'px');
+    $('#board').append(this.$orb);
+  };
+
+  Orb.prototype.release = function () {
+    this.$orb.css('opacity', 1).removeClass('clicked');
+  };
+
+  Orb.prototype.updatePos = function (newPos) {
+    this.pos = newPos;
+    this.$orb.css('top', this.imageWidth * this.pos[0] + 'px');
+    this.$orb.css('left', this.imageWidth * this.pos[1] + 'px');
+  };
+})();
