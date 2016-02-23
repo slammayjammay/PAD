f2b3ede1b17006049abb876518b7401069726c83
Scott McKenzie <slammayjammay@gmail.com>
2016-02-22 22:44:10 -0800

    Change orbs when using textarea

--- painter.js
+++ painter.js
@@ -11,22 +11,54 @@
     this.displayOptions();
   };
 
+  Painter.keyCodes = {
+    8:  'backspace',
+    37: true, // left arrow
+    38: true, // up arrow
+    39: true, // right arrow
+    40: true, // down arrow
+    66: 'b',
+    68: 'd',
+    71: 'g',
+    72: 'h',
+    76: 'l',
+    82: 'r'
+  };
+
   Painter.prototype.changeColor = function (e) {
     this.color = $(e.currentTarget).find('img').attr('color');
-    this.$hoverOrb.attr('src', Orb.colorUrls[this.color]);
+    this.$hoverOrb.attr('src', Orb.colors[this.color]);
   };
 
   Painter.prototype.displayOptions = function () {
-    Object.keys(Orb.colorUrls).forEach(function (color) {
+    Object.keys(Orb.colors).forEach(function (color) {
       var $li = $('<li>').addClass('orb-paint');
 
-      var src = Orb.colorUrls[color];
+      var src = Orb.colors[color];
       var $orb = $('<img>').attr('src', src);
       $orb.attr('color', color);
 
       $li.append($orb);
       $('#paint-colors').append($li);
     });
+  };
+
+  Painter.prototype.keydown = function (e) {
+    var action = Painter.keyCodes[e.keyCode];
+    if (Orb.colors[action]) {
+      var row = 4 - ~~(e.currentTarget.selectionStart / 6) ;
+      var col = e.currentTarget.selectionStart % 6;
+      var orb = this.game.board.orbAtPosition(row, col);
+
+      orb && orb.setColor(action);
+    } else if (action === 'backspace') {
+      if (e.currentTarget.selectionStart === e.currentTarget.selectionEnd) {
+        e.currentTarget.selectionStart -= 1;
+        e.preventDefault();
+      }
+    } else if (!action) {
+      e.preventDefault();
+    }
   };
 
   Painter.prototype.hoverOrbUpdate = function (e) {
@@ -49,6 +81,9 @@
 
     $('.orb-paint').click(this.changeColor.bind(this));
     $('#board').click(this.paintOrb.bind(this));
+
+    $('textarea').keydown(this.keydown.bind(this));
+
     $(window).mousemove(this.hoverOrbUpdate.bind(this));
   };
 
@@ -67,6 +102,7 @@
     this.$hoverOrb.hide();
     this.$el.find('.button').text('Painter');
     this.$el.find('#paint-content').addClass('hidden');
+    this.$el.find('textarea').val('');
 
     this.mouseEventsDisable();
     this.game.mouseEventsEnable();
