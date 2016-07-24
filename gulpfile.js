1ce88b7acd5ad57023e1c0fac0b71e51c91420af
Scott McKenzie <slammayjammay@gmail.com>
2016-07-23 23:10:49 -0700

    Do the CSS

--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,17 @@ var gulp = require('gulp');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
+var sass = require('gulp-sass');
+var cleancss = require('gulp-clean-css');
 
 gulp.task('build', function() {
   gulp.src('./dist/built.js')
-    .pipe(concat('built.js'))
     .pipe(uglify())
     .pipe(gulp.dest('./dist/'));
 
-  gulp.src('./src/style.css')
-    .pipe(rename('built.css'))
-    .pipe(gulp.dest('./dist'))
+  gulp.src('./dist/built.css')
+    .pipe(cleancss())
+    .pipe(gulp.dest('./dist'));
 });
 
 gulp.task('default', ['build']);
