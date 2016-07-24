3ed411a913a8fda7f68cb4eaca201d7ba060f7eb
Scott McKenzie <slammayjammay@gmail.com>
2016-07-23 17:48:38 -0700

    Yayy css

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,17 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
 
 gulp.task('build', function() {
   gulp.src('./dist/built.js')
     .pipe(concat('built.js'))
     .pipe(uglify())
     .pipe(gulp.dest('./dist/'));
+
+  gulp.src('./src/style.css')
+    .pipe(rename('built.css'))
+    .pipe(gulp.dest('./dist'))
 });
 
 gulp.task('default', ['build']);
