dc34f0a28d382a023c238c8e1a3a36b0d70b3ca6
slammayjammay <slammayjammay@gmail.com>
2017-03-06 13:52:25 -0800

    Fix indentation

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,32 @@
 module.exports = {
-  entry: './src/js/index.js',
-  output: {
-    path: __dirname + './dist/',
-    publicPath: '/dist/',
-    filename: 'built.js'
-  },
-  devtool: 'source-map',
-  resolve: {
-    extensions: ['.js', '.jsx', '*']
-  },
-  module: {
-    loaders: [
-      {
-        loader: 'babel-loader',
-        test: [/\.jsx?$/],
-        exclude: /(node_modules)/,
-        query: {
-          presets: ['es2015', 'react', 'stage-2']
-        }
-      },
-      {
-        test: /\.scss$/,
-        use: [
-          { loader: 'style-loader' },
-          { loader: 'css-loader' },
-          { loader: 'sass-loader' }
-        ]
-      }
-    ]
-  }
+	entry: './src/js/index.js',
+	output: {
+		path: __dirname + './dist/',
+		publicPath: '/dist/',
+		filename: 'built.js'
+	},
+	devtool: 'source-map',
+	resolve: {
+		extensions: ['.js', '.jsx', '*']
+	},
+	module: {
+		loaders: [
+			{
+				loader: 'babel-loader',
+				test: [/\.jsx?$/],
+				exclude: /(node_modules)/,
+				query: {
+					presets: ['es2015', 'react', 'stage-2']
+				}
+			},
+			{
+				test: /\.scss$/,
+				use: [
+					{ loader: 'style-loader' },
+					{ loader: 'css-loader' },
+					{ loader: 'sass-loader' }
+				]
+			}
+		]
+	}
 };
